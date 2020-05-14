638893dfaf49654866fd02c5356b712a8fbd5d4d
rediska0123 <rediska0123@gmail.com>
2020-05-14 16:53:32 +0300

    add hw

--- src/Regexp.hs
+++ src/Regexp.hs
@@ -14,7 +14,21 @@ match :: Regexp -> String -> Bool
 match r s = nullable (foldl (flip derivative) r s)
 
 derivative :: Char -> Regexp -> Regexp
-derivative = error "derivative not implemented"
+derivative c Empty     = Empty
+derivative c Epsilon   = Empty
+derivative c (Char x)  = if x == c then Epsilon else Empty
+derivative c (Seq a b) = let
+    f = Seq (derivative c a) b
+    s = derivative c b
+  in
+     if nullable a then Alt f s else f
+derivative c (Alt a b) = Alt (derivative c a) (derivative c b)
+derivative c (Star a)  = Seq (derivative c a) (Star a)
 
 nullable :: Regexp -> Bool
-nullable = error "nullable not implemented"
+nullable Empty     = False
+nullable Epsilon   = True
+nullable (Char x)  = False
+nullable (Seq a b) = nullable a && nullable b
+nullable (Alt a b) = nullable a || nullable b
+nullable (Star a)  = True
